d09fc0159ef1474d07d15924a2059bfb5ca35e91
Jahongir <zokirov123404321@gmail.com>
2022-11-27 15:33:10 +0500

    added testing

--- src/main/java/com/example/projecttesting/model/dto/StudentRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.projecttesting.model.dto;
-
-public record StudentRequest(String name,
-                             String email,
-                             String password) {
-}
